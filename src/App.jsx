e6582c7fe126c1f2307699e21ade4a9f65f3848b
Thien Bao <thienbaomk147@gmail.com>
2026-03-25 16:46:53 +0700

    homework

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState, useEffect } from "react";
+import { Rank } from "./components/Rank/Rank";
+import posts from "./posts.json";
+import { Header } from "./components/Header/Header";
+import "./App.css";
+import { Button } from "./components/Button/Button";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="block">
+        <div className="button">
+          <Button></Button>
+        </div>
+        <div className="container">
+          <Header></Header>
+          {posts.map((player, index) => (
+            <Rank
+              key={index}
+              Rank={player.rank}
+              Name={player.name}
+              Point={player.points}
+              Team={player.team}
+            ></Rank>
+          ))}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+export const Header = () => {
+  return (
+    <div className="Header">
+      <p className="rank">Rank</p>
+      <p className="name">Name</p>
+      <p className="point">Point</p>
+      <p className="team">Team</p>
+    </div>
+  );
+};
