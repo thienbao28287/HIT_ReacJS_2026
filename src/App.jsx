2d5cd4f068be870111d2545cdbf13a62903120f5
Pham Thien Bao <thienbaomk147@gmail.com>
2026-04-22 17:13:05 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
 import { useState, useEffect } from "react";
 import { Header } from "./components/Header/Header";
 import { Main_Content } from "./components/Main_Content/Main_Content";
-
+import { Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
-  return (
-    <>
-      <div>
-        <Header></Header>
-        <Main_Content></Main_Content>
-      </div>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
